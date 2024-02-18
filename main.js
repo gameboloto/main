a1e660f401bab3ededb9e1c259cb034580faca3f
gameboloto <160308674+gameboloto@users.noreply.github.com>
2024-02-18 07:02:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,13 @@
  
     const FETCH_URL = API_BASE_URL + SERVER_URL;
  
-    // Just a cleaner way to store the HTML IDs
     const HTML_IDS = {
         status: "status",
         players: "players",
         motd: "motd",
         icon: "icon",
     };
- 
-    // Same as above
+    
     const HTML_ELEMENT_ENABLED = {
         status: true,
         players: true,
@@ -41,25 +39,17 @@
         let statusEl, playerEl, motdEl, iconEl;
  
         statusEl = HTML_ELEMENT_ENABLED.status ? document.getElementById(HTML_IDS.status) : null;
- 
-        // Note: This is the same as the above line:
-        // if (HTML_ELEMENT_ENABLED.status) {
-        //     statusEl = document.getElementById(HTML_IDS.status);
-        // } else {
-        //     statusEl = null;
-        // }
         
         playerEl = HTML_ELEMENT_ENABLED.players ? document.getElementById(HTML_IDS.players) : null;
         
         //motdEl = HTML_ELEMENT_ENABLED.motd ? document.getElementById(HTML_IDS.motd) : null;
-        
         //iconEl = HTML_ELEMENT_ENABLED.icon ? document.getElementById(HTML_IDS.icon) : null;
  
         if (data.online) {
             
-            statusEl.innerText = statusEl ? ONLINE_TEXT : null;
+            statusEl.innerText = `Статус `+ statusEl ? ONLINE_TEXT : null;
             
-            playerEl.innerText = playerEl ? `${data.players.online}/${data.players.max}` : null;
+            playerEl.innerText = playerEl ? `Игроков ${data.players.online}/${data.players.max}` : null;
             
             //iconEl.innerHTML = iconEl ? `<img src="${data.icon}">` : null;
             
